091c86b0d6ada149b4e571ec311206c94cccf183
Anguerrand <child.of.notime@gmail.com>
2024-01-28 19:15:59 +0000

    коммит1

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://546828d8-b5c4-4aa2-bca7-718fa88d8ab3.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_PATH = "/api/v1/orders/track?t="
---
+++ sender_stand_request.py
@@ -0,0 +1,14 @@
+import configuration
+import requests
+# import data
+
+
+# 1. Создание заказа
+def post_new_order(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH, json=body)
+
+
+# 2. Получение заказа по параметру
+def get_order_by_parameter(track_number):
+    track_with_id = str(track_number)
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_PATH + track_with_id)
